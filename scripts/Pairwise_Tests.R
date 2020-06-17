9522a85da72dd77c3a47a9d21bc256ef2b93f5cc
Adam Faranda <abf@udel.edu>
2020-06-16 21:54:53 -0400

    In scripts/Pairwise_Tests.R updated main analysis loop to run exact and QLF Tests

--- scripts/Pairwise_Tests.R
+++ scripts/Pairwise_Tests.R
@@ -77,25 +77,38 @@ f<-function(z, gr){
   (names(gr)[sapply(gr, function(x, y=z) y %in% x)])[1]
 }
 groups=list(
-  Control=c('1_S1', '2_S2', '4_S3'),
-  Treatment=c('6_S4', '7_S5', '8_S6')
+  Control=c('S1', 'S2', 'S3'),
+  Treatment=c('S4', 'S5', 'S6'),
+  Other=c('S7', 'S8', 'S9')
 )
 dge<-master[,unlist(groups)]
-dge$samples$group<-sapply(dge$samples$sample, f, gr=groups)
+dge$samples$group<-factor(
+  sapply(dge$samples$sample, f, gr=groups),
+  levels = names(groups)
+)
 
-
-# Add Average FPKMs to gene table in master DGEList
+# Add Average FPKMs to gene table in DGEList
 for(g in names(groups)){
   cn<-paste(g, "Avg", sep="_")
   s<-groups[[g]]
   dge$genes[cn] <- apply(rpkm(dge)[,s], 1, mean)   
 }
 
+
+group<-factor(
+  sapply(unlist(groups), f, gr=groups),
+  levels = names(groups)
+)
+design<-model.matrix(~0+group)
+
+
+
 # Define A list of named contrasts; each element points to a vector with
 # a pair of group labels. Positive fold changes will be associated
 # with the second group listed. 
 contrasts=list(
-  Treatment_vs_Control=c('Control', 'Treatment')
+  Treatment_vs_Control=c('Control', 'Treatment'),
+  Other_vs_Treatment=c('Treatment', 'Other')
 )
 
 contrast_descriptions<-list(
@@ -113,90 +126,118 @@ calcFC<-function(lfc){
 	else { return(0)}
 }
 
+# Prepare DGEList for Differential Expression Analysis
+design<-model.matrix(~0+group, dge$samples)     # Define Experimental Design
+colnames(design)<-gsub(
+  'group', '', 
+  colnames(design)
+)
+dge<-dge[filterByExpr(dge, design), ,keep.lib.sizes=F]  # drop low count genes
+dge<-calcNormFactors(dge)                          # Calculate Scaling Factors
+dge<-estimateDisp(dge, design, robust = T)               # Estimate Dispersion
+fit<-glmQLFit(dge, design)                    # Fit QLF Model to design matrix
+
+## Generate diagnostic plots. 
+png("results/BCV_Plot.png")
+plotBCV(dge)                                                 # BCV Plot
+dev.off()
+
+png(
+  "results/PCA_Plot.png",   # PCA Plot
+  width=600, height = 400
+)
+plotPrinComp(
+  cpm(dge, log=T), ft=dge$samples,                  
+  idCol = 0, groupCol = 'group'
+)
+dev.off()
+
+png(
+  "results/MDS_Plot.png",  # MDS Plot
+  width=600, height=500
+)
+par(mar=c(8.5,5,4.1,2.1))
+plotMDS(
+  dge, cex=1.5, cex.lab = 1.5,
+  cex.axis = 1.5, cex.main = 2,
+  pch = rep(15:18, each=3),
+  main = "Experiment Title",
+  col = rep(c('red', 'blue','black'), each=3)
+)
+legend(
+  -3, -2.4, 
+  legend = unique(dge$samples$group), 
+  pch = 15:17, cex = 1.5,
+  col=c('red', 'blue','black'), xpd=T
+)
+dev.off()
+
 # Calculate Differential Expression based on 
 for( c in names(contrasts)){
+  cn<-sapply(
+    colnames(design), 
+    function(n){
+      ifelse(
+        n == contrasts[[c]][1], -1, 
+        ifelse(n == contrasts[[c]][2], 1, 0
+        )
+      )
+    }
+  )
   
-  # dg1 = ag_master %>% filter(
-  #   Experiment == pairwise[[p]]['Experiment'] &
-  #     Group_1 == pairwise[[p]]['Group_1'] &
-  #     Group_2 == pairwise[[p]]['Group_2']
-  # )
   gr1<-groups[[contrasts[[c]][1]]]
   gr2<-groups[[contrasts[[c]][2]]]
   print(paste("Contrast:",c))
   print(paste("Group 1:",paste(gr1, collapse=", ")))
   print(paste("Group 2:",paste(gr2, collapse=", ")))
-  
-  # Define Grouping factor
-  grp<-factor(
-    c(
-      rep(1, length(gr1)), 
-      rep(2, length(gr2))
-    ), 
-    labels=c(contrasts[[c]][1], contrasts[[c]][2]),
-    levels=c(1,2)
-  )
-  names(grp)<-c(gr1, gr2)
-  
-  # Get subset of samples for this contrast and fill design matrix
-  dge<-master[,c(gr1, gr2)]
+  print(contrasts[[c]])
+  print(cn)
 
-  dge$samples$group<-grp[c(gr1, gr2)]
-  design<-model.matrix( ~group, dge$samples)
-  colnames(design)<-gsub('group', '', colnames(design))
-  
-  # Filter DGEList: Remove genes where fewer than two samples have a cpm > 2
-  # Reccommended by EdgeR manual
-  keep <- filterByExpr(dge, design)
-  dge.filter <-dge[keep,,keep.lib.sizes=F]
-  
-  # Calculate Normalization factors and dispersion estimates
-  dge.filter <-calcNormFactors(dge.filter)
-  dge.filter <-estimateDisp(dge.filter, design, robust=T, verbose=T)
-  
-  # Calculate Differential Expression
-  fit<-glmQLFit(dge.filter, design, robust=T)
-  qlf<-glmQLFTest(fit, coef = 2)
-  degSet<-topTags(qlf, n=Inf)@.Data[[1]]
-  degSet$gene_id<-row.names(degSet) # Necessary for downstream dplyr processing
-  
-  degSet$Group_1 <-contrasts[[c]][1]
-  degSet$Group_2 <-contrasts[[c]][2]
-  
-  Avg1 <- paste(contrasts[[c]][1], 'Avg', sep='_')
-  Avg2 <- paste(contrasts[[c]][2], 'Avg', sep='_')
-  cols = c(
-    'gene_id', 'SYMBOL', 'DESCRIPTION', 'logFC', 'PValue',
-    'FDR', Avg1, Avg2, 'Group_1', 'Group_2'
-  )
+  # Estimate Differential Expression
 
-
-  createDEGSpreadSheet(
-    C1 = c,                          # Name of the contrast
-    dg1 = degSet,                    # Data Set for the contrast
-    dg1.bioFun = bioSigRNASeq,       # Biological significance filter for dg1
-    dg1.fdr = "FDR",                 # Statistic used to filter genes for dg1
-    dg1.lfc = "logFC",               # Column in dg1 with log Fold Changes
-    dg1.Avg1 = Avg1,                 # Column in dg1 with average value for Group_1
-    dg1.Avg2 = Avg2,                 # Column in dg1 with average value for Group_2
-    dg1.me = 2,                      # Min. expression for dg1.bioFun
-    dg1.x = 23,                      # row number, corner of dg1 Summary table
-    dg1.y = 2,                       # col number, corner of dg1 Summary table
-    dg1.ds = contrast_descriptions[[c]], # short description for contrast C1 (dg1) 
-    template = "scripts/deg_template.xlsx",
-    descPageName="Data Description", # Name of sheet to write summary tables
-    wb = NULL,                       # Optionally pass a workbook object instead.
-    pref = "" ,                      # Prefix for output file.
-    fname=NULL,                      # Manually specify an output file name
-    use_lfc = FALSE,                 # Whether to use logFC or Fold_Change
-    cols=setdiff(                    # Names of columns to keep in final tables
-      cols, 
-      c('Group_1', 'Group2')
-    ),
-    sc_cols=c("PValue", "FDR")
-  )
+  deg.et<-as.data.frame(topTags(exactTest(dge, pair = contrasts[[c]]), n=Inf))
+  deg.qt<-as.data.frame(topTags(glmQLFTest(fit,contrast=cn), n=Inf))
   
-  # Write full DEG List to a text file
-  fn<-paste(wd,"/results/",c,"_","DEG_Table.tsv", sep="")
-  write.table(degSet[,cols], file=fn, sep="\t", quote=F, row.names = F )
+  fn<-paste(
+    "results/Experiment_",c,"_Exact_Test_DEG.csv", sep="")
+  write.csv(deg.et, fn)
+  
+  fn<-paste(
+    "results/Experiment_",c,"_QLFTest_DEG.csv", sep="")
+  write.csv(deg.qt, fn)
+  
+  # 
+  # 
+  # Avg1 <- paste(contrasts[[c]][1], 'Avg', sep='_')
+  # Avg2 <- paste(contrasts[[c]][2], 'Avg', sep='_')
+  # cols = c(
+  #   'gene_id', 'SYMBOL', 'DESCRIPTION', 'logFC', 'PValue',
+  #   'FDR', Avg1, Avg2, 'Group_1', 'Group_2'
+  # )
+
+  # createDEGSpreadSheet(
+  #   C1 = c,                          # Name of the contrast
+  #   dg1 = degSet,                    # Data Set for the contrast
+  #   dg1.bioFun = bioSigRNASeq,       # Biological significance filter for dg1
+  #   dg1.fdr = "FDR",                 # Statistic used to filter genes for dg1
+  #   dg1.lfc = "logFC",               # Column in dg1 with log Fold Changes
+  #   dg1.Avg1 = Avg1,                 # Column in dg1 with average value for Group_1
+  #   dg1.Avg2 = Avg2,                 # Column in dg1 with average value for Group_2
+  #   dg1.me = 2,                      # Min. expression for dg1.bioFun
+  #   dg1.x = 23,                      # row number, corner of dg1 Summary table
+  #   dg1.y = 2,                       # col number, corner of dg1 Summary table
+  #   dg1.ds = contrast_descriptions[[c]], # short description for contrast C1 (dg1) 
+  #   template = "scripts/deg_template.xlsx",
+  #   descPageName="Data Description", # Name of sheet to write summary tables
+  #   wb = NULL,                       # Optionally pass a workbook object instead.
+  #   pref = "" ,                      # Prefix for output file.
+  #   fname=NULL,                      # Manually specify an output file name
+  #   use_lfc = FALSE,                 # Whether to use logFC or Fold_Change
+  #   cols=setdiff(                    # Names of columns to keep in final tables
+  #     cols, 
+  #     c('Group_1', 'Group2')
+  #   ),
+  #   sc_cols=c("PValue", "FDR")
+  # )
+  # 
 }
